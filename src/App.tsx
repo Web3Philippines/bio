08baee7ab011f577729ab3f1cead76715cdb0296
Kristian Quirapas <kmquirapas@gmail.com>
2022-10-29 09:30:45 +0800

    tweak: update logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import "./App.css";
 import styled from "styled-components";
-import { Discord, Facebook, Instagram, Linkedin, Reddit, Tiktok, Twitter, Youtube } from '@icons-pack/react-simple-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAddressBook, faEnvelope, faVrCardboard } from '@fortawesome/free-solid-svg-icons';
+import {
+  Discord,
+  Facebook,
+  Instagram,
+  Linkedin,
+  Reddit,
+  Tiktok,
+  Twitter,
+  Youtube,
+} from "@icons-pack/react-simple-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAddressBook,
+  faEnvelope,
+  faVrCardboard,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <StyledScope>
       <StyledMain>
         <img src="assets/images/logo.png" alt="Web 3 Philippines Logo" />
-        <h1>Web3 Philippines</h1>
         <h2>Helping Filipinos to build in the Web3 space!</h2>
         <section>
           <a
@@ -17,84 +29,101 @@ function App() {
             target="_blank"
             rel="noreferrer noopener"
           >
-            <FontAwesomeIcon icon={faAddressBook} className='icon-style' size="lg" /> Directory
+            <FontAwesomeIcon
+              icon={faAddressBook}
+              className="icon-style"
+              size="lg"
+            />{" "}
+            Directory
           </a>
           <a
             href="mailto:sudo@web3philippines.org?subject=GM%2C%20Web3%20Philippines%20Team!&body=LPG%20mga%20kabatak!"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <FontAwesomeIcon icon={faEnvelope} className='icon-style' size="lg" /> Contact Us
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              className="icon-style"
+              size="lg"
+            />{" "}
+            Email Us
           </a>
           <a
             href="https://web3philippines.org/discord"
-            target="_blank"className='icon-style'
+            target="_blank"
+            className="icon-style"
             rel="noreferrer noopener"
           >
-            <Discord color='#fff' size={24} className='icon-style'/> Discord
+            <Discord color="#fff" size={24} className="icon-style" /> Discord
           </a>
           <a
             href="https://facebook.com/web3phl"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <Facebook color='#fff' size={24} className='icon-style'/> Page
+            <Facebook color="#fff" size={24} className="icon-style" /> Page
           </a>
           <a
             href="https://facebook.com/groups/web3philippines"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <Facebook color='#fff' size={24} className='icon-style'/> Group
+            <Facebook color="#fff" size={24} className="icon-style" /> Group
           </a>
           <a
             href="https://twitter.com/web3phl"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <Twitter color='#fff' size={24} className='icon-style'/> Twitter
+            <Twitter color="#fff" size={24} className="icon-style" /> Twitter
           </a>
           <a
             href="https://app.gather.town/app/EgdJnVXXhokxeu8g/web3philippines"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <FontAwesomeIcon icon={faVrCardboard} className='icon-style' size="lg" /> Gather
+            <FontAwesomeIcon
+              icon={faVrCardboard}
+              className="icon-style"
+              size="lg"
+            />{" "}
+            Gather
           </a>
           <a
             href="https://linkedin.com/company/web3phl"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <Linkedin color='#fff' size={24} className='icon-style'/> Linkedin
+            <Linkedin color="#fff" size={24} className="icon-style" /> Linkedin
           </a>
           <a
             href="https://instagram.com/web3phl/"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <Instagram color='#fff' size={24} className='icon-style'/> Instagram
+            <Instagram color="#fff" size={24} className="icon-style" />{" "}
+            Instagram
           </a>
           <a
             href="https://reddit.com/r/web3philippines"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <Reddit color='#fff' size={24} className='icon-style'/> Reddit
+            <Reddit color="#fff" size={24} className="icon-style" /> Reddit
           </a>
           <a
             href="https://www.youtube.com/channel/UC1lc-7tbvRiOxspdsn6SCHA"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <Youtube color='#fff' size={24} className='icon-style'/> YouTube
+            <Youtube color="#fff" size={24} className="icon-style" /> YouTube
           </a>
           <a
             href="https://www.tiktok.com/@web3phl"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <Tiktok color='#fff' size={24} className='icon-style'/> Tiktok
+            <Tiktok color="#fff" size={24} className="icon-style" /> Tiktok
           </a>
         </section>
       </StyledMain>
@@ -136,7 +165,9 @@ const StyledMain = styled.main`
   color: white;
 
   img {
-    width 150px;
+    width: 100%;
+    max-width 270px;
+    margin-bottom: 2em;
   }
 
   h1 {
