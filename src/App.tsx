158f26bce810215eba768460a5ea95427d00efc7
Waren Gonzaga <github@warengonzaga.com>
2022-11-10 03:21:32 +0800

    ✨ TWEAK: add nas.io and telegram link

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
   Instagram,
   Linkedin,
   Reddit,
+  Telegram,
   Tiktok,
   Twitter,
   Youtube,
@@ -14,6 +15,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faAddressBook,
   faEnvelope,
+  faUserGroup,
   faVrCardboard,
 } from "@fortawesome/free-solid-svg-icons";
 
@@ -22,8 +24,10 @@ function App() {
     <StyledScope>
       <StyledMain>
         <img src="assets/images/logo.png" alt="Web 3 Philippines Logo" />
-        <h2>Helping Filipinos to build in the Web3 space!</h2>
+        <h2>Helping Filipinos build in Web3 space! 🚀</h2>
         <section>
+
+          {/* Directory */}
           <a
             href="https://directory.web3philippines.org"
             target="_blank"
@@ -36,6 +40,8 @@ function App() {
             />{" "}
             Directory
           </a>
+
+          {/* Email */}
           <a
             href="mailto:sudo@web3philippines.org?subject=GM%2C%20Web3%20Philippines%20Team!&body=LPG%20mga%20kabatak!"
             target="_blank"
@@ -48,6 +54,8 @@ function App() {
             />{" "}
             Email Us
           </a>
+
+          {/* Discord Server */}
           <a
             href="https://web3philippines.org/discord"
             target="_blank"
@@ -56,13 +64,8 @@ function App() {
           >
             <Discord color="#fff" size={24} className="icon-style" /> Discord
           </a>
-          <a
-            href="https://facebook.com/web3phl"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <Facebook color="#fff" size={24} className="icon-style" /> Page
-          </a>
+          
+          {/* Facebook Group */}
           <a
             href="https://facebook.com/groups/web3philippines"
             target="_blank"
@@ -70,6 +73,40 @@ function App() {
           >
             <Facebook color="#fff" size={24} className="icon-style" /> Group
           </a>
+
+          {/* Nas.io */}
+          <a
+            href="https://nas.io/web3philippines"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FontAwesomeIcon
+              icon={faUserGroup}
+              className="icon-style"
+              size="lg"
+            />{" "}
+            Nas.io
+          </a>
+
+          {/* Telegram */}
+          <a
+            href="https://t.me/web3philippines"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Telegram color="#fff" size={24} className="icon-style" /> Telegram
+          </a>
+
+          {/* Facebook Page */}
+          <a
+            href="https://facebook.com/web3phl"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Facebook color="#fff" size={24} className="icon-style" /> Page
+          </a>
+
+          {/* Twitter */}
           <a
             href="https://twitter.com/web3phl"
             target="_blank"
@@ -77,6 +114,8 @@ function App() {
           >
             <Twitter color="#fff" size={24} className="icon-style" /> Twitter
           </a>
+
+          {/* Gather.Town */}
           <a
             href="https://app.gather.town/app/EgdJnVXXhokxeu8g/web3philippines"
             target="_blank"
@@ -89,6 +128,8 @@ function App() {
             />{" "}
             Gather
           </a>
+
+          {/* LinkedIn */}
           <a
             href="https://linkedin.com/company/web3phl"
             target="_blank"
@@ -96,14 +137,18 @@ function App() {
           >
             <Linkedin color="#fff" size={24} className="icon-style" /> Linkedin
           </a>
+
+          {/* Instagram */}
           <a
-            href="https://instagram.com/web3phl/"
+            href="https://instagram.com/web3phl"
             target="_blank"
             rel="noreferrer noopener"
           >
             <Instagram color="#fff" size={24} className="icon-style" />{" "}
             Instagram
           </a>
+
+          {/* Reddit */}
           <a
             href="https://reddit.com/r/web3philippines"
             target="_blank"
@@ -111,15 +156,19 @@ function App() {
           >
             <Reddit color="#fff" size={24} className="icon-style" /> Reddit
           </a>
+
+          {/* YouTube */}
           <a
-            href="https://www.youtube.com/channel/UC1lc-7tbvRiOxspdsn6SCHA"
+            href="https://youtube.com/@web3phl"
             target="_blank"
             rel="noreferrer noopener"
           >
             <Youtube color="#fff" size={24} className="icon-style" /> YouTube
           </a>
+
+          {/* Tiktok */}
           <a
-            href="https://www.tiktok.com/@web3phl"
+            href="https://tiktok.com/@web3phl"
             target="_blank"
             rel="noreferrer noopener"
           >
